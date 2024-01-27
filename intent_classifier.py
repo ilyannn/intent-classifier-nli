6cae8d89e84e8310eb784e3698caf8e6ea3d6bb6
Ilya Nikokoshev <ilya.nikokoshev@gmail.com>
2024-01-27 22:14:48 +0100

    chore: run black

--- intent_classifier.py
+++ intent_classifier.py
@@ -3,6 +3,7 @@
 import pickle
 from sklearn.tree import DecisionTreeClassifier
 
+
 class IntentClassifier:
     def __init__(self):
         self.model = None
@@ -11,21 +12,21 @@ class IntentClassifier:
         return self.model is not None
 
     def load(self, file_path):
-        with open(file_path, 'rb') as f:
+        with open(file_path, "rb") as f:
             self.model = pickle.load(f)
         try:
-            assert isinstance(self.model, dict) 
-            assert 'tree' in self.model and 'words' in self.model
-            assert isinstance(self.model['tree'], DecisionTreeClassifier)
+            assert isinstance(self.model, dict)
+            assert "tree" in self.model and "words" in self.model
+            assert isinstance(self.model["tree"], DecisionTreeClassifier)
         except AssertionError:
-            raise ValueError("Unexpected model format")    
+            raise ValueError("Unexpected model format")
 
     def classify(self, utterance):
-        utterance = utterance.lower().replace('?', '')
-        predict = self.model['tree'].predict
-        words = set(utterance.split(' '))
-        return [predict([[int(word in words) for word in self.model['words']]])[0]]
+        utterance = utterance.lower().replace("?", "")
+        predict = self.model["tree"].predict
+        words = set(utterance.split(" "))
+        return [predict([[int(word in words) for word in self.model["words"]]])[0]]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
--- server.py
+++ server.py
@@ -7,49 +7,61 @@ from flask import Blueprint, Flask, request, jsonify
 
 from intent_classifier import IntentClassifier
 
-api = Blueprint('main', __name__)
+api = Blueprint("main", __name__)
 model = IntentClassifier()
 
 
-@api.route('/ready')
+@api.route("/ready")
 def ready():
     if model.is_ready():
-        return 'OK', 200
+        return "OK", 200
     else:
-        return 'Not ready', 423
+        return "Not ready", 423
 
 
-@api.route('/intent', methods=['POST'])
+@api.route("/intent", methods=["POST"])
 def intent():
     if not request.is_json:
-        return jsonify({
-            "label": "BODY_MISSING",
-            "message": "Request doesn't have a body."
-        }), 400
+        return (
+            jsonify(
+                {"label": "BODY_MISSING", "message": "Request doesn't have a body."}
+            ),
+            400,
+        )
 
     data = request.get_json()
-    if not isinstance(data, dict) or 'text' not in data:
-        return jsonify({
-            "label": "TEXT_MISSING",
-            "message": "\"text\" missing from request body."
-        }), 400
+    if not isinstance(data, dict) or "text" not in data:
+        return (
+            jsonify(
+                {
+                    "label": "TEXT_MISSING",
+                    "message": '"text" missing from request body.',
+                }
+            ),
+            400,
+        )
 
     try:
-        intents = model.classify(data['text'])
-        return jsonify({'intents ': [{'label': label} for label in intents]}), 200
+        intents = model.classify(data["text"])
+        return jsonify({"intents ": [{"label": label} for label in intents]}), 200
     except Exception as e:
-        return jsonify({
-            "label": "INTERNAL_ERROR",
-            "message": f"Something went wrong while processing the request: {e}"
-        }), 500
+        return (
+            jsonify(
+                {
+                    "label": "INTERNAL_ERROR",
+                    "message": f"Something went wrong while processing the request: {e}",
+                }
+            ),
+            500,
+        )
 
 
-DEFAULT_MODEL_PATH = os.getenv('MODEL')
+DEFAULT_MODEL_PATH = os.getenv("MODEL")
 
 
 def create_app(model_path=DEFAULT_MODEL_PATH):
     if not model_path:
-        raise ValueError('Please provide model path as a MODEL environment variable')
+        raise ValueError("Please provide model path as a MODEL environment variable")
     app = Flask(__name__)
     app.register_blueprint(api)
     model.load(model_path)
@@ -58,14 +70,21 @@ def create_app(model_path=DEFAULT_MODEL_PATH):
 
 def main():
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('--model', type=str, default=DEFAULT_MODEL_PATH, required=not DEFAULT_MODEL_PATH,
-                            help='Path to model directory or file.')
-    arg_parser.add_argument('--port', type=int, default=os.getenv('PORT', 8080), help='Server port number.')
+    arg_parser.add_argument(
+        "--model",
+        type=str,
+        default=DEFAULT_MODEL_PATH,
+        required=not DEFAULT_MODEL_PATH,
+        help="Path to model directory or file.",
+    )
+    arg_parser.add_argument(
+        "--port", type=int, default=os.getenv("PORT", 8080), help="Server port number."
+    )
     args = arg_parser.parse_args()
 
     app = create_app(args.model)
     app.run(port=args.port)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
